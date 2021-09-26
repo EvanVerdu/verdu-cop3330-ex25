64e81c749ef296ec52784e5e273d5f3ec612ace8
EvanVerdu <89754847+EvanVerdu@users.noreply.github.com>
2021-09-26 17:51:56 -0400

    Add files via upload

---
+++ verdu-cop3330-ex25/src/main/java/ex25/ex25.java
@@ -0,0 +1,76 @@
+package ex25;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Evan Verdu
+ */
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class ex25 {
+
+    public static void main (String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int strength;
+        String StrengthString = " ";
+
+        System.out.print("Please enter your password: ");
+        String password = scanner.nextLine();
+
+
+        strength = PasswordValidator(password);
+
+        if(strength == 0){
+            System.out.print("Please enter a new password.");
+            System.exit(0);
+        }
+        else if (strength == 1){
+            StrengthString = " very weak ";
+        }
+        else if (strength == 2){
+            StrengthString = " weak ";
+        }
+        else if (strength == 3){
+            StrengthString = " strong ";
+        }
+        else if (strength == 4){
+            StrengthString = " very strong ";
+        }
+
+        System.out.print("The password " + "'" + password + "'" + "is a" + StrengthString + "password.");
+    }
+
+    public static int PasswordValidator(String password){
+
+         int length = password.length();
+         int value;
+
+         Pattern p = Pattern.compile("[^a-zA-Z0-9]");
+         Matcher m = p.matcher(password);
+
+         if (password.matches("[0-9]+") && length < 8){
+             value = 1;
+             return value;
+         }
+         else if (password.matches("[a-zA-Z]+") && length < 8){
+             value = 2;
+             return value;
+         }
+         else if (password.matches("[A-Za-z0-9]+") && length >= 8) {
+             value = 3;
+             return value;
+         }
+
+         else if (m.find() && length >= 8) {
+              value = 4;
+              return value;
+         }
+
+         return 0;
+    }
+
+
+}
